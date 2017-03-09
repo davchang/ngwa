f348f0d59aaef9a3fa5c8642ce3a4cc7b4d56773
Heiko Frost <hfrost@strongtie.com>
2017-03-09 10:41:08 -0800

    Added basic Bootstrap styles

--- .webpack.config.js
+++ .webpack.config.js
@@ -11,71 +11,75 @@ const environment = process.env.NODE_ENV || 'development';
 const WEBPACK_DEV_SERVER = "webpack-dev-server/client?http://localhost:8080";
 
 const entry = {
-  app: [ "./index.js" ]
+	app: [ "./index.js" ]
 };
 let devtool = 'source-map';
 
 if (environment !== "production") {
-  entry.app = entry.app.concat([ WEBPACK_DEV_SERVER ]);
-  devtool = 'cheap-module-eval-source-map';
+	entry.app = entry.app.concat([ WEBPACK_DEV_SERVER ]);
+	devtool = 'cheap-module-eval-source-map';
 }
 
 module.exports = {
 
-  devtool,
+	devtool,
 
-  entry,
+	entry,
 
-  eslint: {
-    configFile: path.join(__dirname, '.eslintrc.js'),
-    emitError: true,
-    emitWarning: true,
-    failOnError: false,
-    failOnWarning: false
-  },
+	eslint: {
+		configFile: path.join(__dirname, '.eslintrc.js'),
+		emitError: true,
+		emitWarning: true,
+		failOnError: false,
+		failOnWarning: false
+	},
 
-  module: {
-    loaders: [
-      {
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        test: /\.js$/
-      },
-      {
-        exclude: /node_modules/,
-        loader: 'url-loader',
-        test: /(\.svg|\.png)$/
-      },
-      {
-        loader: 'json-loader',
-        test: /\.json$/
-      },
-      {
-        exclude: /node_modules/,
-        loader: ExtractTextPlugin.extract('css!postcss!sass'),
-        test: /\.scss$/
-      },
-      {
-        include: path.resolve('app'),
-        loader: 'isparta-loader',
-        test: /\.js$/
-      }
-    ]
-  },
+	module: {
+		loaders: [
+			{
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				test: /\.js$/
+			},
+			{
+				exclude: /node_modules/,
+				loader: 'url-loader',
+				test: /(\.svg|\.png)$/
+			},
+			{
+				loader: 'json-loader',
+				test: /\.json$/
+			},
+			{
+				exclude: /node_modules/,
+				loader: ExtractTextPlugin.extract('css!postcss!sass'),
+				test: /\.scss$/
+			},
+			{
+				include: path.resolve('app'),
+				loader: 'isparta-loader',
+				test: /\.js$/
+			},
+			{
+				test: /\.(eot|ttf|woff|woff2)$/,
+				loader: 'file?name=[name].[ext]&publicPath=assets/fonts/'
+			}
+		]
+	},
 
-  plugins: [
-    new ExtractTextPlugin('dist/[name].css'),
-    new webpack.DefinePlugin({
-      "process.env": { 'NODE_ENV': JSON.stringify(environment) }
-    })
-  ],
+	plugins: [
+		new ExtractTextPlugin('dist/[name].css'),
+		new webpack.DefinePlugin({
+			"process.env": { 'NODE_ENV': JSON.stringify(environment) }
+		})
+	],
 
-  output: {
-    filename: "dist/[name].js"
-  },
+	output: {
+		filename: "dist/[name].js"
+	},
 
-  postcss: [
-    autoprefixer({ browsers: [ 'last 3 versions' ] })
-  ]
+	postcss: [
+		autoprefixer({ browsers: [ 'last 3 versions' ] })
+	]
 
 };
--- src/library/button/index.js
+++ src/library/button/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 class Button extends React.Component {
   render() {
     console.log("--- in Button render ---");
-    return (<button className="button">Click Me!</button>);
+    return (<button className="button"><span className="icon--new"></span> Click Me!</button>);
   }
 }
 
